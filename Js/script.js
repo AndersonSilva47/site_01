32b66af087f7f697c561cd909f9a43d6e39df9f3
Anderson Silva <Andpita47@gmail.com>
2021-10-10 16:01:28 -0300

    alterações slider e nova area para imagens do slide

--- Js/script.js
+++ Js/script.js
@@ -2,12 +2,14 @@
 let qtdSlide = document.querySelectorAll('.img-slide').length;
 let widthSlide = document.querySelector('.slider').clientWidth;
 let heightSlide = document.querySelector('.slider').clientHeight;
+let widthDaily = document.querySelector('.hot-daily').clientWidth;
 
 document.querySelector('.control-slide').style.width = `${widthSlide}px`; //CONFIGURAÇÃO PARA ESPAÇO FIXO;
 document.querySelector('.control-slide').style.height = `${heightSlide}px`;
 
 document.querySelector('.slot-slide').style.width = `${widthSlide * qtdSlide}px`;
 document.querySelector('.promotion').style.width = `${widthSlide}px`;
+document.querySelector('.promotion2').style.width = `${widthDaily}px`;
 
 let currentSlide = 0;
 
@@ -35,3 +37,19 @@ function updateMargin() {
 }
 
 setInterval(goNext, 4000);
+
+//
+
+let img1 = document.querySelector('.img-slide.img1').style.backgroundImage;
+let img2 = document.querySelector('.img-slide.img2').style.backgroundImage;
+let img3 = document.querySelector('.img-slide.img3').style.backgroundImage;
+let img4 = document.querySelector('.img-slide.img4').style.backgroundImage;
+let img5 = document.querySelector('.img-slide.img5').style.backgroundImage;
+
+
+
+document.querySelector('.mini-img.img1').style.backgroundImage = img1;
+document.querySelector('.mini-img.img2').style.backgroundImage = img2;
+document.querySelector('.mini-img.img3').style.backgroundImage = img3;
+document.querySelector('.mini-img.img4').style.backgroundImage = img4;
+document.querySelector('.mini-img.img5').style.backgroundImage = img5;
